b887f092818709d022a12d556c7003e6913da0a1
Rahul Mewada <58613121+Rahul-Mewada@users.noreply.github.com>
2021-04-27 16:35:24 -0400

    Time: 408 ms (53.49%), Space: 15 MB (22.73%) - LeetHub

--- task-scheduler/task-scheduler.py
+++ task-scheduler/task-scheduler.py
@@ -1,24 +1,32 @@
+'''
+[A A A A B B B B C C] n = 3
+[B1 C2]
+A---A---A---A
 
+1) Keep track of the maximum freq
+2) num_slots = n * (freq - 1) and gaps = max_freq - 1
+3) fill the slots with the next max freq
+'''
 
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         if n == 0:
             return len(tasks)
-        
         freq = [0] * 26
         
         for task in tasks:
             freq[ord(task) - ord('A')] += 1
+        freq.sort(reverse=True)
+        
+        max_freq = freq[0]
+        gaps = max_freq - 1 
+        idle_time = n * gaps
+        
+        for index in range(1, len(freq)):
+            cur_freq = freq[index]
+            idle_time -= min(gaps, cur_freq)
             
-        freq.sort()    
-        num_spaces = freq[-1] - 1
-        idle_slots = num_spaces * n
+        idle_time = max(idle_time, 0)
+        return idle_time + len(tasks)
         
-        for index in range(len(freq)-2, -1, -1):
-            idle_slots -= min(num_spaces, freq[index])
-        
-        if idle_slots > 0:
-            return idle_slots + len(tasks) 
-        else:
-            return len(tasks)
