769cf7267144469ac1d5e7398a05c0e4dafc3cbb
Rahul Mewada <58613121+Rahul-Mewada@users.noreply.github.com>
2021-04-27 12:08:29 -0400

    Time: 96 ms (97.13%), Space: 14.4 MB (11.14%) - LeetHub

--- 3sum-closest/3sum-closest.py
+++ 3sum-closest/3sum-closest.py
@@ -1,43 +1,35 @@
 '''
-Three sum problem 
-
-1) sort the array - O(n log n)
-2) start a loop
-3) Nest the two pointers in that loop
-4) Keep track of the sum closest to the target
-
-[-4, -2, -1, 0] target = 1   
-  i   l      r
+[-4, -1, 1, 2]
+  i   i  l  r
+1) Sort the array
+2) Start a loop 
+3) Inside the loop initiate two pointers
 '''
-
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if len(nums) <= 3:
+            result = 0
+            for num in nums:
+                result += num
+            return result
+        
         nums.sort()
-        last_sum = None
-        print(nums)
+        diff = 100000
+        result = diff
+        
         for index, num in enumerate(nums):
-            left = index + 1
-            right = len(nums) - 1
-            
+            left = index+1
+            right = len(nums)-1
             while left < right:
-                three_sum = num + nums[left] + nums[right]      
-                cur_error = abs(target - three_sum)                 
-                if last_sum != None: 
-                    last_error = abs(target - last_sum)
-                    if cur_error < last_error:
-                        last_sum = three_sum
-                else:
-                    last_sum = three_sum                            
-                    
-                if three_sum == target:
-                    return target
-                elif three_sum < target:
+                summ = num + nums[left] + nums[right]
+                if abs(summ - target) <= diff:
+                    result = summ
+                    diff = abs(summ - target)
+                if summ > target:
+                    right -= 1
+                elif summ < target:
                     left += 1
                 else:
-                    right -= 1
-                print(last_sum) 
-                print()
-                    
-              
-               
-        return last_sum
+                    return summ
+        return result
+                
