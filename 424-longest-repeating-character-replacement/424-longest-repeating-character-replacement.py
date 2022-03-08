2d8a15482880b6c53f0e0e13e9a5fbb23b528e53
Rahul Mewada <58613121+Rahul-Mewada@users.noreply.github.com>
2022-03-08 11:47:26 -0600

    Time: 705 ms (5.00%), Space: 13.9 MB (74.51%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,45 @@
+"""
+we need a way to find out if the substring is valid, ie - that the freq of chars that are not repeating are <= k
+    - keep track of the freq of 'main' char, ie - the char with the highest freq
+    - (highest_freq - size of the window) gives us freq of non repeating chars- we want this to be <= k
+    - adjust the window based off this
+    
+    
+ A  k = 10
+sf
+
+cur_freq = {
+    A : 1
+}
+
+max_len = 1
+"""
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        slow = 0
+        cur_freq = {}
+        max_len = 0
+        for fast in range(len(s)):
+            if s[fast] in cur_freq:
+                cur_freq[s[fast]] += 1
+            else:
+                cur_freq[s[fast]] = 1
+            
+            while ((fast - slow + 1) - self.freq_max_repeating(cur_freq, k)) > k:
+                if cur_freq[s[slow]] == 1:
+                    cur_freq.pop(s[slow])
+                else:
+                    cur_freq[s[slow]] -= 1
+                slow += 1
+            
+            max_len = max(max_len, fast - slow + 1)
+        return max_len
+    
+        
+    def freq_max_repeating(self, cur_freq, k):
+        max_freq = -float('inf')
+        for key, value in cur_freq.items():
+            max_freq = max(max_freq, value)
+        
+        return max_freq
